41902cac5cb8af565eb47cd35b485822fdff4f79
trishacastillo <79441676+trishacastillo@users.noreply.github.com>
2022-09-15 17:17:05 +0800

    ;)

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,35 @@ import MovieList from "./components/MovieList";
 import SearchBox from "./components/Searchbox";
 import useFetch from "./useFetch";
 import ReactLoading from "react-loading";
+import TvShows from "./TvShows";
 
 const Home = () => {
   const [search, setSearch]=useState([]);
+   
   const API_KEY='ae7f7ed289b6d77c7761b724b99082dc';
   const popular=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_video=true&page=1`;
-  const url=`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${search}`;
-  const { error, isPending, data: movies } = useFetch(popular)
-  const { data: searchData } = useFetch(url)
+  const urlSearch=`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${search}`;
+  const [url, setUrl]=useState([popular]);
+ 
+  //const { error, isPending, data: movies } = useFetch(popular)
+  const { error, isPending, data: searchData } = useFetch(url)
   useEffect(()=>{
     setSearch(search);
   },[search]);
+
+  function searchClick() {
+    if (search !== undefined || search !== null || search=='') {
+      setUrl(urlSearch)
+    }else{
+      setUrl(popular)}
+  }
+
 
 
   return (
     <div className="home">
       <SearchBox search={search} setSearch={setSearch}/>
+      <button onClick={searchClick}>Search</button>
       { error && <div>{ error }</div> }
       { isPending && 
         <div className="loaders">
@@ -27,7 +40,7 @@ const Home = () => {
           size={10} className="loaders"/>
         </div> }
       { searchData && <div><h2>Search Movies</h2> <MovieList movies={searchData}/></div> }
-      {searchData ? (<div></div>):(<div>{ movies && <div><h2>Trending Movies</h2> <MovieList movies={movies}/></div> }</div>) }
+      <TvShows/>
     </div>
     
   );
